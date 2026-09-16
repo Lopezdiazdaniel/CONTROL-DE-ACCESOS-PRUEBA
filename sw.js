b3c082b37f1c74ac32d4f6525d1dbc1b81e0904e
Lopezdiazdaniel <62415013+Lopezdiazdaniel@users.noreply.github.com>
2026-09-15 18:46:11 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('acceso-pwa-v1').then((cache) => {
+      return cache.addAll([
+        './index.html',
+        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
